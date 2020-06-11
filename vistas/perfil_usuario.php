a3e5e464d7744f5737d7490a9bc37a9b725ee966
gerardo <kaliceh@gmail.com>
2020-06-11 10:30:21 -0500

    Creamos el archivo perfil_usuario.php

---
+++ vistas/perfil_usuario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <title>Gerardo Rivera</title>
+    <?php require_once 'auxiliares/encabezado.php' ?>
+</head>
+<body>
+    <?php require_once 'auxiliares/navbar.php' ?>
+
+
+
+    <?php require_once 'auxiliares/footer.php' ?>
+</body>
+</html>
