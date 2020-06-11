463ac5a82db89b84905c7e3b79e7d68b514470f2
gerardo <kaliceh@gmail.com>
2020-06-11 10:06:21 -0500

    Agregamos el título a la página

--- vistas/auxiliares/navbar.php
+++ vistas/auxiliares/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar sticky-top navbar-expand-md bg-dark navbar-dark">
-    <a class="navbar-brand" href="#"><img src="../img/logo.jpg" alt="log_equipo" style="width:50px; border-radius:10px;" /></a>
+    <a class="navbar-brand" href="#"><img src="../img/logo.jpg" alt="log_equipo" style="width:50px; border-radius:10px;"/> Compras en caliente</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
     </button>
