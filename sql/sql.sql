637ded61340e80bd367e23626191daca94c4a9ca
gerardo <kaliceh@gmail.com>
2020-06-11 09:26:12 -0500

    Insertamos precio, y cantidad a productos. Modificamos fechas en compras_hechas SQL

--- sql/sql.sql
+++ sql/sql.sql
@@ -14,14 +14,16 @@ CREATE TABLE productos(
     nombre_producto VARCHAR(50) NOT NULL,
     descripcion_producto TEXT,
     imagen_producto VARCHAR(50) NOT NULL,
+    precio REAL NOT NULL,
+    cantidad_productos INT(50),
     PRIMARY KEY(id_producto)
 );
 CREATE TABLE compras_hechas(
   id_compra_hecha INT(20)  NOT NULL AUTO_INCREMENT,
   fk_email_usuario INT(20) NOT NULL,
   fk_producto INT(20) NOT NULL,
-  fecha_del_pedido VARCHAR(50) NOT NULL,
-  fecha_de_entrega VARCHAR(50) NOT NULL,
+  fecha_del_pedido DATE NOT NULL,
+  fecha_de_entrega DATE NOT NULL,
   PRIMARY KEY (id_compra_hecha),
   foreign key (fk_email_usuario) REFERENCES usuarios_registrados(email_usuario),
   foreign key (fk_producto) REFERENCES productos(id_producto)
