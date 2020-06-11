cfeaf8fcc3494d5be241510311d1b9e522d4d2df
gerardo <kaliceh@gmail.com>
2020-06-11 10:12:52 -0500

    Modificamos el carrito

--- vistas/carrito.php
+++ vistas/carrito.php
@@ -22,7 +22,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col mb-4">
+            <!-- <div class="col mb-4">
                 <div class="card">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
@@ -48,7 +48,7 @@
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
     <?php include_once 'auxiliares/footer.php' ?>
