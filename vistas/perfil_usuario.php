042b5e8ca16683b2e3baf18136448238e13be7aa
gerardo <kaliceh@gmail.com>
2020-06-16 18:51:05 -0500

    Realizamos la maquetación del perfil de Gerardo

--- vistas/perfil_usuario.php
+++ vistas/perfil_usuario.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>    
+<head>
     <title>Gerardo Rivera</title>
-    <?php require_once 'auxiliares/encabezado.php' ?>
+    <?php require_once 'auxiliares/encabezado.php'?>
 </head>
 <body>
     <?php require_once 'auxiliares/navbar.php' ?>
-
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <img src="../img/gerardo.jpeg" class="img-circle" alt="Foto_de_perfil">
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        Nombre : <strong>Gerardo</strong><br>
+                        Apellido(s) : <strong>Rivera Ramírez</strong>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">                
+            </div>
+        </div>
+    </div>
     <?php require_once 'auxiliares/footer.php' ?>
 </body>
 </html>
