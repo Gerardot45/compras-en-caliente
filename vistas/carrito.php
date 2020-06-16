91bbbae36fd7e7d981dc512104cd25fe1b29f0b8
gerardo <kaliceh@gmail.com>
2020-06-16 18:53:26 -0500

    Modificamos las vistas de los productos

--- vistas/carrito.php
+++ vistas/carrito.php
@@ -12,13 +12,25 @@
     <?php require_once 'auxiliares/navbar.php'; ?>
     <br><br>
     <div class="container">
+    <div class="container">
+            <center>
+                <button class="btn btn-success">Comprar todo el carrito</button>
+                <button class="btn btn-danger">Quitar todo de la lista</button>
+            </center>
+            <br>
+            <hr>
+        </div>
         <div class="row row-cols-1 row-cols-md-2">
             <div class="col mb-4">
                 <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="../img/iphone11.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">Iphone 11</h5>
+                        <p class="card-text">Descripción del Iphone 11</p>                    
+                        <hr>
+                        <button class="btn btn-success btn-sm">Comprar Artículo</button>
+                         
+                        <button class="btn btn-danger btn-sm">Quitar del carrito</button>
                     </div>
                 </div>
             </div>
@@ -49,7 +61,7 @@
                     </div>
                 </div>
             </div> -->
-        </div>
+        </div>        
     </div>
     <?php include_once 'auxiliares/footer.php' ?>
 </body>
