2748785fa41bda0b8ea1208eb140bb4f0d415ea1
gerardo <kaliceh@gmail.com>
2020-06-16 18:51:55 -0500

    Agregamos los nombres de los integrantes y pusimos MISION y VISION

--- vistas/index.php
+++ vistas/index.php
@@ -9,6 +9,26 @@
     <!-- navbar -->
     <?php include_once 'auxiliares/navbar.php'; ?>
     <br><br>
+    <div class="container">
+        <h1 class="text-center">INICIO</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Nombre de los integrantes</h3>
+                <br>
+                <ul>
+                    <li>Gerardo Rivera Ramírez</li>
+                    <li>Marco Alexis Sánchez Epifanio</li>
+                    <li>Samuel Gasca Fragoso</li>
+                    <li>Ángel Daniel Huerta Tehuacatl</li>
+                </ul>
+            </div>
+            <div class="col-md-8">
+                <h3 class="text-center">Misión</h3>
+                <h3 class="text-center">Visión</h3>
+                <h3 class="text-center">Sobre el pequeño proyecto</h3>
+            </div>
+        </div>
+    </div>
     <?php include_once 'auxiliares/footer.php'?>
 </body>
 </html>
